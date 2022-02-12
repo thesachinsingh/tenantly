d23be2ba0ccf694e212c649659f9811ffb54eb21
Sachin Singh <49466686+thesachinsingh@users.noreply.github.com>
2022-02-12 23:53:57 +0530

    created db models

---
+++ models.py
@@ -0,0 +1,39 @@
+from enum import unique
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+
+db = SQLAlchemy()
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key = True, nullable = False)
+    username = db.Column(db.String, nullable = False, unique = True)
+    pw = db.Column(db.String, nullable = False)
+
+class Tenants(db.Model):
+    tenant_id = db.Column(db.Integer, primary_key = True, nullable = False)
+    tenant_name = db.Column(db.String, nullable = False, unique = True)
+    tenant_room_no = db.Column(db.String, nullable = False, unique = True)
+    tenant_rent = db.Column(db.Integer, nullable = False)
+    tenant_electricity_rate = db.Column(db.Integer, default = 10)
+    tenant_join_date = db.Column(db.DateTime)
+    
+    def __repr__(self):
+        return '<Tenant %r>' % self.tenant_name
+
+
+
+class TenantRecords(db.Model):
+    __tablename__ = 'tenant_records'
+    transaction_id = db.Column(db.Integer, primary_key = True, nullable = False)
+    tenant_id = db.Column(db.integer, db.ForeignKey("tenants.tenant_id"), nulable = False)
+    month = db.Column(db.String, nullable = False)
+    prev_bal = db.Column(db.Integer, nullable = False)
+    curr_month_charges = db.Column(db.Integer, nullable = False)
+    money_recieved = db.Column(db.Integer, nullable = False)
+    curr_bal = db.Column(db.Integer, nullable = False)
+
+class Generate(db.Model):
+    tenant_id = db.Column(db.Integer, db.ForeignKey("tenants.tenant_id"), nullable = False)
+    generated_date = db.Column(db.DateTime, nullable = False)
+    for_month = db.Column(db.String, nullable = False)
+
+
