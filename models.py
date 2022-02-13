f2086f96aacdbebcaf390ab65a31e3d8c08b1fed
Sachin Singh <49466686+thesachinsingh@users.noreply.github.com>
2022-02-13 23:52:30 +0530

    added relationships to make stuff easier

--- models.py
+++ models.py
@@ -15,7 +15,8 @@ class Tenants(db.Model):
     tenant_rent = db.Column(db.Integer, nullable = False)
     tenant_electricity_rate = db.Column(db.Integer, default = 10)
     tenant_join_date = db.Column(db.DateTime)
-    
+    monthly_receipt = db.relationship("Generate", backref = "tenant_details")
+
     def __repr__(self):
         return '<Tenant %r>' % self.tenant_name
 
@@ -27,13 +28,20 @@ class TenantRecords(db.Model):
     tenant_id = db.Column(db.integer, db.ForeignKey("tenants.tenant_id"), nulable = False)
     month = db.Column(db.String, nullable = False)
     prev_bal = db.Column(db.Integer, nullable = False)
-    curr_month_charges = db.Column(db.Integer, nullable = False)
+
+    curr_month_charges = db.Column(db.Integer, db.ForeignKey("generate.bill"), nullable = False)
+    
     money_recieved = db.Column(db.Integer, nullable = False)
     curr_bal = db.Column(db.Integer, nullable = False)
+    tenant_details = db.relationship("Tenants", backref = "transactions")
+    monthly_receipt = db.relationship("Generate", backref = "all_transactions")
+
+    
 
 class Generate(db.Model):
     tenant_id = db.Column(db.Integer, db.ForeignKey("tenants.tenant_id"), nullable = False)
     generated_date = db.Column(db.DateTime, nullable = False)
     for_month = db.Column(db.String, nullable = False)
+    #tenant_details relationship with Tenants table
 
 
